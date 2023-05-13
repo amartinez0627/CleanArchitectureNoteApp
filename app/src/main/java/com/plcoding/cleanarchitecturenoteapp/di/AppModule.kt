42ed14ed2a8fd90f432e14d65f81ec7fd291436b
Antonio Martinez <antoniomartinez12347@gmail.com>
2023-05-12 22:10:23 -0600

    implemented NotesViewModel with events and state. Also added AddNoteUseCase

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
 import com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.AddNoteUseCase
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.DeleteNoteUseCase
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotesUseCase
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
@@ -38,8 +39,9 @@ object AppModule {
     @Singleton
     fun provideNoteUseCases(repository: NoteRepository): NoteUseCases{
         return NoteUseCases(
-            getNotes = GetNotesUseCase(repository),
-            deleteNoteUseCase = DeleteNoteUseCase(repository)
+            getNotesUseCase = GetNotesUseCase(repository),
+            deleteNoteUseCase = DeleteNoteUseCase(repository),
+            addNoteUseCase = AddNoteUseCase(repository)
         )
     }
 }
--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,6 +1,7 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 
 data class NoteUseCases(
-    val getNotes: GetNotesUseCase,
-    val deleteNoteUseCase: DeleteNoteUseCase
+    val getNotesUseCase: GetNotesUseCase,
+    val deleteNoteUseCase: DeleteNoteUseCase,
+    val addNoteUseCase: AddNoteUseCase
 )
